c91c8daee33a05e2bd27a062004803a112c865d1
Ryan Chan <ryan@ryanchan.info>
2022-06-17 00:09:41 -0500

    first commit

---
+++ Server/Config/db.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Secret = exports.HostName = exports.RemoteURI = exports.LocalURI = void 0;
+exports.LocalURI = "mongodb://127.0.0.1/media";
+exports.RemoteURI = "mongodb+srv://ryanchan:4FdQR3q37NdndBnV@cluster0.l76hs.mongodb.net/media?retryWrites=true&w=majority";
+exports.HostName = "MongoDB Atlas";
+exports.Secret = "someSecret";
+//# sourceMappingURL=db.js.map
---
+++ Server/Config/db.ts
@@ -0,0 +1,4 @@
+export const LocalURI = "mongodb://127.0.0.1/media";
+export const RemoteURI = "mongodb+srv://ryanchan:4FdQR3q37NdndBnV@cluster0.l76hs.mongodb.net/media?retryWrites=true&w=majority";
+export const HostName = "MongoDB Atlas";
+export const Secret = "someSecret";
